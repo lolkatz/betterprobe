906dd08c6d8c86aa2752744ca5c807d89416af8c
lolkatz <80469043+lolkatz@users.noreply.github.com>
2024-10-29 11:54:08 -0400

    Create betterprobe.sh
    initial commit

---
+++ betterprobe.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while IFS= read -r url; do
+
+        # Check if the site is up and get HTTP status code
+        http_status=$(curl -o /dev/null -s -L -w "%{http_code}" "$url")
+
+        if [ "$http_status" -eq "000" ]; then
+                echo "Error connecting"
+                echo "$url"
+                wget --spider --https-only --timeout=15 --tries=1 $url -o error.txt
+                cat error.txt | grep -E "ERROR|failed"
+                echo ""
+        elif [ "$http_status" -ne "200" ]; then
+                echo "Not OK"
+                echo "$url"
+                echo "$http_status"
+                echo ""
+        fi
+done < "$1"
